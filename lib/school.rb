4a3777d851dbd72fcb2839e3ee536260bec59009
Mark Rego <rego121@gmail.com>
2019-08-26 20:18:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+attr_accessor :studentname :grade
+
+
+roster = {}
+
+
+  def initialize(schoolname)
+
+    @schoolname = schoolname
+
+  end
+
+
+  def add_student(studentname, grade)
+
+    roster <<  {:studentname => :grade}
+
+  end
+
+  def grade
+
+  end
+
+end
+
+  
